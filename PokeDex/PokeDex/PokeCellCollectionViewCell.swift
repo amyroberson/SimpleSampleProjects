1e429d42500789511afdcbe18a6799d65c4f5662
Amy Roberson <amy@roberson.xyz>
2017-04-19 16:29:18 -0400

    UI set up

---
+++ PokeDex/PokeDex/PokeCellCollectionViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  PokeCellCollectionViewCell.swift
+//  PokeDex
+//
+//  Created by Amy Roberson on 4/19/17.
+//  Copyright © 2017 Amy Roberson. All rights reserved.
+//
+
+import UIKit
+
+class PokeCellCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImage: UIImageView!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    required init?(coder aDecoder: NSCoder){
+        super.init(coder: aDecoder)
+        
+        layer.cornerRadius = 5.0
+    }
+    
+    func configureCell(pokemon: Pokemon){
+        self.pokemon = pokemon
+        
+        nameLabel.text = self.pokemon.name.capitalized
+        thumbImage.image = UIImage(named: "\(self.pokemon.pokedexId)")
+    }
+}
