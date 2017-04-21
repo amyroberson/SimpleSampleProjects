627e6bcfec44ba07eca0435821b3451483cb756c
Amy Roberson <amy@roberson.xyz>
2017-04-21 11:48:28 -0400

    updated model

--- PokeDex/PokeDex/PokemonDetailVC.swift
+++ PokeDex/PokeDex/PokemonDetailVC.swift
@@ -12,7 +12,18 @@ class PokemonDetailVC: UIViewController {
 
     var pokemon: Pokemon!
     
+    @IBOutlet weak var mainImage: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    @IBOutlet weak var defenseLabel: UILabel!
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var pokedexLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var attackLabel: UILabel!
+    @IBOutlet weak var evoImage: UIImageView!
+    @IBOutlet weak var nextEvoImage: UIImageView!
+    @IBOutlet weak var evoLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,4 +31,7 @@ class PokemonDetailVC: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func backButttonPressed(_ sender: UIButton) {
+        dismiss(animated:true, completion: nil)
+    }
 }
